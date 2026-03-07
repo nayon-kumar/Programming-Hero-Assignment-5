8b09a71ec11226c75e4259d935d51029508f3838
nayon-kumar <nayon.cse55@gmail.com>
2026-03-07 14:30:06 +0600

    Login function added

---
+++ scripts/login.js
@@ -0,0 +1,13 @@
+const singinBtn = document.getElementById("singinBtn");
+singinBtn.addEventListener("click", function () {
+  const userName = document.getElementById("userName").value.trim();
+  const password = document.getElementById("password").value.trim();
+  if (userName === "admin" && password === "admin123") {
+    window.location.href = "main.html";
+  } else {
+    document.getElementById("userName").value = "";
+    document.getElementById("password").value = "";
+    const my_modal_3 = document.getElementById("my_modal_3");
+    my_modal_3.showModal();
+  }
+});
